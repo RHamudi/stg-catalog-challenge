52e4e4e3012db944286387e001fd78cacb8a267d
RHamudi <ramonramos.silva16@gmail.com>
2025-08-04 20:01:34 -0300

    feat: adding products with api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,33 @@
+"use client"
+import { useEffect } from "react";
+import Navbar from "./components/navbar"
+import ProductCard from "./components/productCard"
+import { IProduct } from "./types";
+import { UserAuth } from "./context/authContext";
 
 export default function Home() {
+
+  const { products } = UserAuth();
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <Navbar></Navbar>
+      <div className="min-h-screen bg-gradient-to-br from-green-50 to-white pt-30 ">
+        <div className="max-w-6xl mx-auto">
+          <h1 className="text-4xl font-bold text-green-800 text-center mb-2">
+            Nossos Produtos
+          </h1>
+          <p className="text-green-600 text-center mb-12">
+            Descubra nossa seleção de produtos sustentáveis e de alta qualidade
+          </p>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            {products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
